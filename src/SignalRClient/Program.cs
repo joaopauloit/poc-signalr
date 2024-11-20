57ee9cadffc6a878d05c82a5beda5357b56d55d6
João Paulo R.S <joaopauloit@gmail.com>
2024-11-20 17:32:14 -0300

    feat(poc): PoC simples de uso signalR

---
+++ src/SignalRClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5254/chat") // URL do servidor SignalR
+    .Build();
+
+connection.On<string, string>("ReceiveMessage", (user, message) =>
+{
+    Console.WriteLine($"{user}: {message}");
+});
+
+try
+{
+    await connection.StartAsync();
+    Console.WriteLine("Conexão estabelecida com o SignalR.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao conectar: {ex.Message}");
+}
+
+while (true)
+{
+    Console.WriteLine("Digite seu nome:");
+    var user = Console.ReadLine();
+    Console.WriteLine("Digite sua mensagem:");
+    var message = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(user) && !string.IsNullOrWhiteSpace(message))
+    {
+        await connection.InvokeAsync("SendMessage", user, message);
+    }
+}
